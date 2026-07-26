365b89267f97793267d24321c2bb5b3dafc6c41b
Iranzi Response <iranziresponse@gmail.com>
2026-07-26 09:13:41 +0300

    Make Orch site feel more human

--- site/assets/js/site.js
+++ site/assets/js/site.js
@@ -14,20 +14,3 @@
     }, { threshold: 0.15, rootMargin: '0px 0px -40px 0px' });
     targets.forEach(function (el) { observer.observe(el); });
 })();
-
-(function () {
-    var prefersReducedMotion = window.matchMedia('(prefers-reduced-motion: reduce)').matches;
-    var heroFigure = document.querySelector('.hero-figure');
-    var tilt = document.querySelector('.laptop-tilt');
-    if (prefersReducedMotion || !heroFigure || !tilt) return;
-
-    heroFigure.addEventListener('mousemove', function (event) {
-        var bounds = heroFigure.getBoundingClientRect();
-        var relX = (event.clientX - bounds.left) / bounds.width - 0.5;
-        var relY = (event.clientY - bounds.top) / bounds.height - 0.5;
-        tilt.style.transform = 'rotateY(' + (relX * 8) + 'deg) rotateX(' + (relY * -8) + 'deg)';
-    });
-    heroFigure.addEventListener('mouseleave', function () {
-        tilt.style.transform = '';
-    });
-})();
